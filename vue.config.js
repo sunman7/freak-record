51955443dfec4f55169e0eeacc4a0b22066de4a4
sunman7 <473910527@qq.com>
2021-01-23 11:11:16 +0800

    引入echarts，修复BUG

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@
 const path = require("path");
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/orange-website/'
+        : '/',
+
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, "src/assets/icons");
