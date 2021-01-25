4856f4fef1ff24bd53bfb2eb24dd410ea9be0a1d
sunman7 <473910527@qq.com>
2021-01-25 12:09:19 +0800

    完成二维码

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:sunman7/orange-website.git master:gh-pages
+ git push -f git@gitee.com:liamhuangzzz/orange-website.git master:gh-pages
 
 cd -
